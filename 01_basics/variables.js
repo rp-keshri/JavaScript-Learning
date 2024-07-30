8030d381fdd9db9ff1b341b46eb5fd7a5108f67d
rp-keshri <144160038+rp-keshri@users.noreply.github.com>
2024-07-30 09:48:18 +0000

    varialbe concept ok

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -3,4 +3,17 @@ let name = "Rohit";
 var tittle = "Keshri";
 address = "Ramgarh";
 
+// {
+//     var x = "jjdj";
+//     let y = "hhghkhjs";
+// }
+
+// console.log(x)
+// console.log(y)
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 console.table([score,name,tittle,address]);
